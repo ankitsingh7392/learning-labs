78794a7a2e32de5eb0f818dd206782e3f8c8dc57
Ankit Singh <ankitsingh7392@gmail.com>
2026-03-05 23:01:58 +0000

    add itr code

---
+++ python/01-iterator/example-01.py
@@ -0,0 +1,17 @@
+arr = [1, 2, 3, 4, 5, 6]
+it = iter(arr)
+print(next(it))
+print(next(it))
+print(next(it))
+print(next(it))
+print(next(it))
+print(next(it))
+print(next(it)) # It will not run
+"""
+Traceback (most recent call last):
+  File "example-01.py", line 9, in <module>
+    print(next(it))
+          ^^^^^^^^
+StopIteration
+"""
+
